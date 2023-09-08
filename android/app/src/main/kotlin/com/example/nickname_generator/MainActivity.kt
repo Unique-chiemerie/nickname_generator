7d913115a9b41dc83967a6d17d071493dae5fee7
user is blnk <132100200+Unique-chiemerie@users.noreply.github.com>
2023-09-08 22:37:48 +0100

    nickname generator

---
+++ android/app/src/main/kotlin/com/example/nickname_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nickname_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: MainApp(),
+    ),
+  );
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
