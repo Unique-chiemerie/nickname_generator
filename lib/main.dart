59353fa4a4d39b971dcf35cc340c270b7931a153
user is blnk <132100200+Unique-chiemerie@users.noreply.github.com>
2023-09-09 15:07:57 +0100

    added cool features

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,32 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   List<String> finalnames = [];
 //this is the function that does the magic
+  bool isloading = false;
+  bool spun = false;
+  final ScrollController scrcontroller = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    scrcontroller.addListener(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    void generator() {
+      setState(() {
+        isloading = true;
+        finalnames.add(
+          ' ${adjectives[Random().nextInt(34)]} ${mainname[Random().nextInt(30)]}',
+        );
+      });
+//simulation of delay
+      Future.delayed(const Duration(seconds: 5), () {
+        setState(() {
+          isloading = false;
+        });
+      });
+    }
+
     return Scaffold(
       backgroundColor: Colors.black12,
       body: Column(
@@ -42,31 +65,71 @@ class _MainAppState extends State<MainApp> {
             height: 70,
           ),
           SizedBox(
-            height: 300,
+            height: 50,
+            width: 50,
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  finalnames.clear();
+                  spun = !spun;
+                });
+              },
+              icon: spun
+                  ? const Icon(
+                      Icons.refresh,
+                      color: Colors.purple,
+                      size: 50,
+                    )
+                  : const Icon(
+                      Icons.refresh_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+            ),
+          ),
+          SizedBox(
+            height: 450,
             width: double.infinity,
             child: ListView.builder(
+              controller: scrcontroller,
               itemCount: finalnames.length,
               itemBuilder: (context, index) {
-                return TilE(
-                  tex: finalnames[index],
-                );
+                //function for button here
+
+                if (isloading = false) {
+                  return const SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else {
+                  return TilE(
+                    tex: finalnames[index],
+                  );
+                }
               },
             ),
           ),
           const Spacer(),
           ElevatedButton(
             onPressed: () {
-              setState(() {
-                finalnames.add(
-                  adjectives[Random().nextInt(34)] +
-                      mainname[Random().nextInt(30)],
-                );
-              });
+              generator();
+              scrcontroller.animateTo(scrcontroller.position.maxScrollExtent,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.fastOutSlowIn);
             },
             child: const Text('Generate'),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    scrcontroller.dispose();
   }
 }
